0b7c94ad775e816aa4ab77c42b48596b49b80ec2
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-09-29 17:21:34 -0700

    Point to Desktop Vision

--- src/commands.js
+++ src/commands.js
@@ -3,12 +3,15 @@
  * and registration.
  */
 
-export const AWW_COMMAND = {
-  name: 'awwww',
-  description: 'Drop some cuteness on this channel.',
-};
-
-export const INVITE_COMMAND = {
-  name: 'invite',
-  description: 'Get an invite link to add the bot to your server',
+export const CREDITS_COMMAND = {
+  name: 'credits',
+  description: `Credits keep the games running. Have some free ones on the house once per hour! 50-1500 credits can be won!`,
+  options: [
+    {
+      name: 'email',
+      description: 'Your email address.',
+      type: 3, // 3 is string
+      required: true,
+    },
+  ],
 };
--- src/server.js
+++ src/server.js
@@ -8,9 +8,7 @@ import {
   InteractionType,
   verifyKey,
 } from 'discord-interactions';
-import { AWW_COMMAND, INVITE_COMMAND } from './commands.js';
-import { getCuteUrl } from './reddit.js';
-import { InteractionResponseFlags } from 'discord-interactions';
+import { CREDITS_COMMAND } from './commands.js';
 
 class JsonResponse extends Response {
   constructor(body, init) {
@@ -58,24 +56,32 @@ router.post('/', async (request, env) => {
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
     // Most user commands will come as `APPLICATION_COMMAND`.
     switch (interaction.data.name.toLowerCase()) {
-      case AWW_COMMAND.name.toLowerCase(): {
-        const cuteUrl = await getCuteUrl();
-        return new JsonResponse({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content: cuteUrl,
+      case CREDITS_COMMAND.name.toLowerCase(): {
+        const options = interaction.data.options;
+        if (options.length === 0 || !options[0].value) {
+          return new JsonResponse({ 
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE, 
+            data: { content: 'Email is required.' }
+          });
+        }
+        
+        //send post req to dv api
+        const url =  'https://desktop.vision/api/credits'
+        const key = env.DV_KEY
+
+        const response = await fetch(url, {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': key,
           },
+          method: 'POST',
+          body: JSON.stringify({email: options[0].value}),
         });
-      }
-      case INVITE_COMMAND.name.toLowerCase(): {
-        const applicationId = env.DISCORD_APPLICATION_ID;
-        const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${applicationId}&scope=applications.commands`;
+        const body = await response.json();
+        console.log(body)
         return new JsonResponse({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content: INVITE_URL,
-            flags: InteractionResponseFlags.EPHEMERAL,
-          },
+          data: { content: body.message },
         });
       }
       default:
