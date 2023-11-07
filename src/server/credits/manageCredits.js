a31a006b90178068b89eeb731d730ca4eb41f5e8
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-11-07 14:10:50 -0800

    fix 404 error

--- src/server/credits/manageCredits.js
+++ src/server/credits/manageCredits.js
@@ -25,13 +25,15 @@ async function manageCredits(interaction, env, type = 'balance') {
 
   const body = await response.json();
 
-  if (response.status !== 400 || 404) {
+  if (response.status === 400 || response.status === 404) {
     const errorEmbed = {
-      title: "Error!",
-      description: body.message || "There was a error running this command. Please try again. If not, please contact support.",
+      title: 'Error!',
+      description:
+        body.message ||
+        'There was a error running this command. Please try again. If not, please contact support.',
       color: 0xff0000,
     };
-  
+
     return new JsonResponse({
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: { embeds: [errorEmbed] },
